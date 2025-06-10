ac91bcfdbb614af1b4ef297d4b34de8eb0143f90
kkomal03 <komal.kumawat@habilelabs.io>
2025-06-10 11:35:15 +0000

    practice

--- day5/practice.js
+++ day5/practice.js
@@ -1,1 +1,52 @@
-console.log("Hello JavaScript");
+// console.log("Hello JavaScript");
+// // let x=5;
+// var y=10;
+// const z= 3;
+// console.log(y+z);
+
+
+
+// let x ={x:"name"} ;
+// console.log(typeof(x));
+// let y = ["name", "emp.id"]
+// console.log(typeof(y));
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(typeof(cars));
+
+
+
+// function myfun(c1,c2){
+//     console.log("hello in function");
+    
+// }
+// myfun();
+// sayName("komal");
+// function sayName(name){
+//     console.log(name);
+// }
+// let x=10;
+
+// x--;
+// console.log(x--);
+
+
+
+
+
+
+
+
+function sumNum(a,b){
+    return a+b;
+}
+let sum = sumNum(4,5);
+console.log(sum);
+
+
+
+
+
+
+
+
