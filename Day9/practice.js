04c0fed2599b83250f901f2e27a9fe39a7689803
kkomal03 <komal.kumawat@habilelabs.io>
2025-06-17 09:39:21 +0000

    loop in js

---
+++ Day9/practice.js
@@ -0,0 +1,16 @@
+let num = 5;
+
+switch (num) {
+    case 0:
+        console.log("Number is zero.");
+        
+    case 1:
+        console.log("Nuber is one.");
+       
+    case 2:
+        console.log("Number is two.");
+       
+    default:
+       
+        console.log("Number is greater than 2.");
+};
