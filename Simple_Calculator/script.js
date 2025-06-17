8dfb84b3db3f1a332fa6815c9bf5e2524e3478cb
kkomal03 <komal.kumawat@habilelabs.io>
2025-06-17 09:41:26 +0000

    add eventlistner,animation

--- Simple_Calculator/script.js
+++ Simple_Calculator/script.js
@@ -5,7 +5,36 @@ function append(value){
 function clearDisplay(){
     display.value=" ";
 }
-function calculate(){
-     display.value = eval(display.value);
+// function calculate(){
+//      display.value = eval(display.value);
 
+// }
+
+
+function  deleteButton(){
+    display.value=display.value.substr(0, -1);
+
+}
+
+
+
+document.addEventListener("keypress",function(event){
+    if(event.key === "Enter"){
+        calculate();
+    }else if(event.key === "Backspace"){
+        deleteButton();
+    }
+});
+function calculate(){
+    try{
+        if(display.value.includes("/0")){
+            alert("cannot divide by zero");
+            clearDisplay();
+        }else{
+            display.value=eval(display.value);
+        }
+    }catch(error){
+        alert("invalid")
+        clearDisplay();
+    }
 }
