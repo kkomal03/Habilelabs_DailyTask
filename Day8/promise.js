e2a46d1e23e3a343b782c03430ffc9ec959b5aaa
kkomal03 <komal.kumawat@habilelabs.io>
2025-06-17 09:37:48 +0000

    practice example of promise

--- Day8/promise.js
+++ Day8/promise.js
@@ -1,3 +1,13 @@
+
+
+// A Promise in JavaScript is an object representing the eventual 
+// completion or failure of an asynchronous operation. 
+// It's a way to handle operations that might take some time to complete, 
+// like fetching data from a server or reading a file. 
+
+// promise.all:runs multiple promises in parallel
+
+
 // Promise.all([
 //   Promise.resolve("One"),
 //   Promise.resolve("Two"),
@@ -5,7 +15,7 @@
 // ]).then((values) => console.log(values)); 
 
 
-
+//.allSettled:returns all results , regardlessnof resolve /reject
 // Promise.allSettled([
 //   Promise.resolve("One"),
 //   Promise.resolve("Two"),
@@ -14,6 +24,8 @@
 
 
 
+
+//Promise.race: returns the results of the first setteld promise(weather fullfiled or rejected)
 // Promise.race([
 //   Promise.resolve("One"),
 //   Promise.resolve("Two"),
